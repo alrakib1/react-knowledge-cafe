42aaee3529f5a29030dac77bdc7b9d1d6ed25ace
alrakib1 <abdullahalrakib30@gmail.com>
2023-09-14 00:27:31 +0600

    new project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blogs from './components/blogs/Blogs'
+import Header from './components/header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
